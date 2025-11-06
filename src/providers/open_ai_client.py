0c7a0573e84d4ae0f293465ca8bcab5f48f5950a
Sendhilishaan <ishaansendhil@gmail.com>
2025-11-05 21:19:18 -0500

    feat: asynchronous call function for openai client

--- src/providers/open_ai_client.py
+++ src/providers/open_ai_client.py
@@ -1,6 +1,30 @@
-#import openai
+from openai import OpenAI
 from . import OPENAI_API_KEY
+from abs import api_client
+import asyncio
 
 """
 open ai api calls
 """
+
+class OpenAI_client(api_client):
+    
+    def __init__(self, model: str):
+        super().__init__(model)
+        self.client = OpenAI(api_key=OPENAI_API_KEY)
+    
+    async def call(self, prompt: str, **kwargs):
+        """
+        Make a call asynchronously by running the synchronous SDK call in a 
+        background thread. Returns the raw response object. This is non-blocking.
+
+        additional arguments will be required for rate limiting maybe
+        """
+        response = await asyncio.to_thread(
+            self.client.chat.completions.create,
+            model=self.model_name,
+            messages=[{"role": "user", "content": prompt}],
+            **kwargs
+            )
+
+        return response
