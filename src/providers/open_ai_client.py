b2da1bc7f5dd3d064306fdadd045e318cc950455
Sendhilishaan <ishaansendhil@gmail.com>
2025-11-15 14:34:30 -0500

    fix: abs client and openAI client class

--- src/providers/open_ai_client.py
+++ src/providers/open_ai_client.py
@@ -1,10 +1,16 @@
 from openai import OpenAI
 from . import OPENAI_API_KEY
-from abs import api_client
+from src.abs.client import api_client
 import asyncio
+from openai import OpenAIError, APIError, RateLimitError, APIConnectionError, Timeout
+from src.abs.prompt import Prompt
 
 """
 open ai api calls
+
+test with:
+python3 -m src.providers.open_ai_client
+
 """
 
 class OpenAI_client(api_client):
@@ -13,18 +19,41 @@ class OpenAI_client(api_client):
         super().__init__(model)
         self.client = OpenAI(api_key=OPENAI_API_KEY)
     
-    async def call(self, prompt: str, **kwargs):
+    async def call(self, prompt: Prompt, **kwargs):
         """
         Make a call asynchronously by running the synchronous SDK call in a 
         background thread. Returns the raw response object. This is non-blocking.
 
         additional arguments will be required for rate limiting maybe
-        """
-        response = await asyncio.to_thread(
-            self.client.chat.completions.create,
-            model=self.model_name,
-            messages=[{"role": "user", "content": prompt}],
-            **kwargs
-            )
 
-        return response
+        returning a tuple (modelname, output) so that we know which model outputs what
+        when we are saving to the json
+
+        """
+        try:
+            response = await asyncio.to_thread(
+                self.client.chat.completions.create,
+                model=self.model_name,
+                messages=[{"role": "user", "content": prompt.text}],
+                **kwargs
+                )
+
+            print(f"Prompt id: {prompt.id} for model: {self.model_name} completed")
+            return (self.model_name, response.choices[0].message.content)
+
+        except Exception as e:
+            print(f"Error: {type(e).__name__}: {e} in prompt id: {prompt.id} in OpenAI client")
+            return (self.model_name, f"Error: {type(e).__name__}: {e}")
+
+async def test():
+    client = OpenAI_client(model="gpt-4o-mini")   
+    prompt = Prompt(id=1, text="Say hello!", category="happy")
+
+    print("Calling API...")
+    result = await client.call(prompt)
+
+    print("Result:\n")
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(test())
